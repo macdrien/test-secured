778b0bae3a9ffb46872198bc27342b5fcdc7c13f
macdrien <adrien.bouyssou@gmail.com>
2024-06-19 21:30:25 +0200

    Initialize project

---
+++ src/test/kotlin/com/example/test_securized/TestSecurizedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.test_securized
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestSecurizedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
