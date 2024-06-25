efa10bf118d84d4bafa2df50454e87a35e278fdd
macdrien <adrien.bouyssou@gmail.com>
2024-06-25 23:01:16 +0200

    ADD Default load doc in IDEA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+	id("idea")
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("plugin.jpa") version "1.9.24"
@@ -32,6 +33,13 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation(kotlin("script-runtime"))
+}
+
+idea {
+	module {
+		isDownloadJavadoc = true
+		isDownloadSources = true
+	}
 }
 
 kotlin {
